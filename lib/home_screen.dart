3b066aaacdf6075356350598dc7300b37876a5c2
Ulises Lira <moredialiraulises13@gmail.com>
2024-02-14 13:52:31 -0600

    Adding ListTile to the ListView

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -11,7 +11,45 @@ class HomeScreen extends StatelessWidget {
       ),
       body: ListView(
         children: const [
-          Text('Hola Bombastic'),
+          ListTile(
+            leading: Icon(Icons.water_drop, color: Colors.blue),
+            title: Text(
+                    'Gotita 1',
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 19,
+                    ),
+                    ),
+            subtitle: Text('Normal'),
+            trailing: Icon(Icons.arrow_circle_right, color: Colors.orange),
+          ),
+          Divider(), //Muestra una línea
+          ListTile(
+            leading: Icon(Icons.water_drop_outlined, color: Colors.cyan), // M,uestra un icono a la izquierda
+            title: Text(
+                    'Gotita 2',
+                    style: TextStyle(
+                      color: Colors.cyan,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 19,
+                    ),),
+            subtitle: Text('Outlined'),
+            trailing: Icon(Icons.arrow_circle_right, color: Colors.orange),
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.water_drop_rounded, color: Colors.teal),
+            title: Text(
+                    'Gotita 3',
+                    style: TextStyle(
+                      color: Colors.teal,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 19,
+                    ),),
+            subtitle: Text('Rounded'),
+            trailing: Icon(Icons.arrow_circle_right, color: Colors.orange),
+          ),
         ],
       ),
     );
