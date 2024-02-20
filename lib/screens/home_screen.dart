f193685bc132712790252d131b998e3ce7656715
Ulises Lira <moredialiraulises13@gmail.com>
2024-02-20 13:41:50 -0600

    Finalizing the app theme

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:practica3/theme/app_theme.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -15,45 +16,51 @@ class HomeScreen extends StatelessWidget {
             leading: const Icon(Icons.input, color: Colors.blue),
             title: Text(
                     'Entradas',
-                    style: Theme.of(context).textTheme.headlineLarge, // Estilos para el título desde el tema de main_app.dart
+                    style: AppTheme.lightTheme.textTheme.headlineLarge, // Estilos para el título desde el tema de main_app.dart
                     // style: TextStyle(
                     //   color: Colors.blue,
                     //   fontWeight: FontWeight.bold,
                     //   fontSize: 19,
                     // ),
                     ),
-            subtitle: const Text('Diferentes widgets para entradas de Flutter'),
-            trailing: const Icon(Icons.arrow_circle_right, color: Colors.orange),
+            subtitle: Text('Diferentes widgets para entradas de Flutter',
+                            style: AppTheme.lightTheme.textTheme.bodySmall,
+                          ),
+            trailing: const Icon(Icons.arrow_circle_right, color: AppTheme.iconColor),
           ),
           const Divider(), //Muestra una línea
           ListTile(
             leading: const Icon(Icons.list, color: Colors.cyan), // M,uestra un icono a la izquierda
             title: Text(
                     'ListView.builder',
-                    style: Theme.of(context).textTheme.headlineLarge, // Estilos para el título desde el tema de main_app.dart
+                    style: AppTheme.lightTheme.textTheme.headlineLarge, // Estilos para el título desde el tema de main_app.dart
                     // style: TextStyle(
                     //   color: Colors.cyan,
                     //   fontWeight: FontWeight.bold,
                     //   fontSize: 19,
                     // ),
                   ),
-            subtitle: const Text('Scroll infinito'),
-            trailing: const Icon(Icons.arrow_circle_right, color: Colors.orange),
+            subtitle: Text('Scroll infinito',
+                                  style: AppTheme.lightTheme.textTheme.bodySmall,
+                          ),
+            trailing: const Icon(Icons.arrow_circle_right, color: AppTheme.iconColor),
           ),
           const Divider(),
           ListTile(
             leading: const Icon(Icons.notification_add, color: Colors.teal),
             title: Text(
                     'Notificaciones',
-                    style: Theme.of(context).textTheme.headlineLarge, // Estilos para el título desde el tema de main_app.dart
+                    style: AppTheme.lightTheme.textTheme.headlineLarge, // Estilos para el título desde el tema de main_app.dart
                     // style: TextStyle(
                     //   color: Colors.teal,
                     //   fontWeight: FontWeight.bold,
                     //   fontSize: 19,
                     // ),
                   ),
-            subtitle: const Text('Creación de notificaciones'),
-            trailing: const Icon(Icons.arrow_circle_right, color: Colors.orange),
+            subtitle: Text('Creación de notificaciones',
+                                  style: AppTheme.lightTheme.textTheme.bodySmall,
+                          ),
+            trailing: const Icon(Icons.arrow_circle_right, color: AppTheme.iconColor),
           ),
           const Divider(),
         ],
--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -5,6 +5,7 @@ class AppTheme {
   // Declaraciones de color
   static const mainColor = Color.fromARGB(255, 210, 216, 252);
   static const backColor = Color.fromARGB(255, 131, 170, 255);
+  static const iconColor = Color.fromARGB(255, 0, 38, 119);
   
   // Declaraciones del tema claro 
   static final ThemeData lightTheme = ThemeData().copyWith(
@@ -16,12 +17,23 @@ class AppTheme {
       headlineLarge: GoogleFonts.tiltNeon( // theNautigal - Letra cursiva 
         // Cambia el estilo del título
         fontWeight: FontWeight.w700, // Cambia el estilo de la fuente
-        color: Colors.teal, // Cambia el color de la fuente
+        color: Colors.indigo, // Cambia el color de la fuente
         fontSize: 16.5, // Cambia el tamaño de la fuente
-        decoration: TextDecoration.lineThrough, // Agrega una línea al texto
+        // decoration: TextDecoration.lineThrough, // Agrega una línea al texto
         // decorationColor: Colors.indigo, // Cambia el color de la decoración
         // decorationStyle: TextDecorationStyle.dotted, // Cambia el estilo de la decoración
         // decorationThickness: 5, // Cambia el grosor de la decoración
+        // fontStyle: FontStyle.italic
+      ),
+      bodySmall: GoogleFonts.dangrek( // theNautigal - Letra cursiva 
+        // Cambia el estilo del subtítulo
+        fontWeight: FontWeight.w500, // Cambia el estilo de la fuente
+        color: Colors.blueGrey, // Cambia el color de la fuente
+        fontSize: 14.0, // Cambia el tamaño de la fuente
+        decoration: TextDecoration.underline, // Agrega una línea al texto
+        decorationColor: Colors.indigo, // Cambia el color de la decoración
+        // decorationStyle: TextDecorationStyle.dotted, // Cambia el estilo de la decoración
+        decorationThickness: 2, // Cambia el grosor de la decoración
         // fontStyle: FontStyle.italic
       ),
     ),
